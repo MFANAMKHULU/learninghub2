efb5ebf7d5ae23be02544a4fb496d5f04189f704
MFANAMKHULU <katlehomofokeng179@gmail.com>
2024-01-29 14:52:59 +0200

    attempting 14:52

--- Asambeni_buses/getimage.php
+++ Asambeni_buses/getimage.php
@@ -17,7 +17,7 @@ if ($mysqli->connect_error) {
 // Get the selected bus company from the POST request
 $selectedBusCompany = $_POST['busCompany'];
 
-// Prepare and execute a query to fetch image file name for the selected bus company
+// Prepare and execute a query to fetch image file names for the selected bus company
 $query = "SELECT image_filename FROM BusCompanies WHERE company_name = ? LIMIT 1";
 $stmt = $mysqli->prepare($query);
 $stmt->bind_param("s", $selectedBusCompany);
@@ -25,39 +25,20 @@ $stmt->execute();
 $stmt->bind_result($imageFilename);
 $stmt->fetch(); // Fetch the image filename
 
-// Close the statement, as we'll use the connection again
+// Close the statement
 $stmt->close();
 
-// Display the image based on the filename
+// Fetch all image filenames for the selected bus company
+$imagePaths = array();
 if ($imageFilename) {
-    
+
     $imagePath = "Asambeni_buses/images/" . $imageFilename;
-    echo "<img src='$imagePath' alt='Bus Company Image'>";
-} else {
-    echo "No image available for the selected bus company.";
+    $imagePaths[] = $imagePath;
 }
 
+// Return JSON-encoded array of image paths
+echo json_encode($imagePaths);
 
-
-// Prepare and execute a query to fetch routes for the selected bus company
-$routeQuery = "SELECT route_id, route_name FROM Routes WHERE company_id = (
-    SELECT company_id FROM BusCompanies WHERE company_name = ? LIMIT 1
-)";
-$routeStmt = $mysqli->prepare($routeQuery);
-$routeStmt->bind_param("s", $selectedBusCompany);
-$routeStmt->execute();
-$routeStmt->bind_result($routeId, $routeName);
-
-// Build HTML options for the route dropdown
-$options = '';
-while ($routeStmt->fetch()) {
-    $options .= "<option value='$routeId'>$routeName</option>";
-}
-
-// Close the statement and database connection
-$routeStmt->close();
+// Close the database connection
 $mysqli->close();
-
-// Return the HTML options for the route dropdown
-echo $options;
 ?>
