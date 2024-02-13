ea041fb01c91d27b1bd8cc310c8d24d28960dde1
MFANAMKHULU <katlehomofokeng179@gmail.com>
2024-02-13 21:59:02 +0200

    appearing comments

--- Asambeni_buses/getreview.php
+++ Asambeni_buses/getreview.php
@@ -1,12 +1,19 @@
 <?php
-
+// getreviews.php
 
 // Include the database connection
 require('db_connect.php');
 
 try {
-    // Prepare and execute SQL query to fetch all reviews
-    $stmt = $pdo->query("SELECT * FROM reviews");
+    // Get the selected company ID from the query parameters
+    $selectedCompanyId = isset($_GET['companyId']) ? $_GET['companyId'] : null;
+
+    // Prepare and execute SQL query to fetch reviews for the selected company
+    $stmt = $pdo->prepare("SELECT * FROM reviews WHERE company_id = :companyId");
+    $stmt->bindParam(':companyId', $selectedCompanyId, PDO::PARAM_INT);
+    $stmt->execute();
+
+    // Fetch reviews from the result set
     $reviews = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     // Return reviews as JSON
